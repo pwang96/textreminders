a5abbb542f0d518f182301ad9f3076929f9f78d5
pwang96 <96pwang@gmail.com>
2017-06-23 12:44:20 -0600

    debug

--- textreminders/forms.py
+++ textreminders/forms.py
@@ -2,5 +2,4 @@ from django import forms
 
 
 class NumberForm(forms.Form):
-    your_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$',
-                                   error_messages="Phone number must be entered in the format: '+999999999'.")
+    your_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$')
